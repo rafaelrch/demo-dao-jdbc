d3e6d61b9a45f2a03678273b0cfc262901de8cb1
Rafael Rocha <115195984+rafaelrch@users.noreply.github.com>
2026-05-26 13:10:33 -0300

    seller entity class

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 import model.entites.Department;
+import model.entites.Seller;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
         Department dpt = new Department(1, "books");
-
+        Seller seller = new Seller(2, "Rafael", "rafael@gmail.com", new Date(), 3000.0, dpt );
+        System.out.println(seller);
         System.out.println(dpt);
     }
 }
