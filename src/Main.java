85ebdcd244875029542957651e20287ebe9b5af1
Rafael Rocha <115195984+rafaelrch@users.noreply.github.com>
2026-05-26 13:10:33 -0300

    SellerDaoJDBC and DaoFactory

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import model.dao.DaoFactory;
+import model.dao.SellerDAO;
 import model.entites.Department;
 import model.entites.Seller;
 
@@ -7,6 +9,9 @@ public class Main {
     public static void main(String[] args) {
         Department dpt = new Department(1, "books");
         Seller seller = new Seller(2, "Rafael", "rafael@gmail.com", new Date(), 3000.0, dpt );
+
+        SellerDAO sellerdao = DaoFactory.createSellerDao();
+
         System.out.println(seller);
         System.out.println(dpt);
     }
