38906782f3667c72e0bc26f775aaa925a536ff94
Rafael Rocha <115195984+rafaelrch@users.noreply.github.com>
2026-05-26 13:10:36 -0300

    delete implemetation

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,11 @@ import model.entites.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
         SellerDAO sellerdao = DaoFactory.createSellerDao();
 
         System.out.println("=== TEST 1 seller findById ===");
@@ -39,5 +41,12 @@ public class Main {
         seller.setName("Rafael Rocha");
         sellerdao.update(seller);
         System.out.println("Update completed");
+
+        System.out.println("=== TEST 6 seller delete ===");
+        System.out.println("Enter id for delete test: ");
+        int id = sc.nextInt();
+        sellerdao.deleteById(id);
+        System.out.println("Delete completed!");
+        sc.close();
     }
 }
